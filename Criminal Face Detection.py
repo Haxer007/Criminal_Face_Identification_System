8826b329946ba5c71a93388de707b29b8b1e96e3
Eswar <73170547+Haxer007@users.noreply.github.com>
2021-07-08 19:41:12 +0530

    Update Criminal Face Detection.py

--- Criminal Face Detection.py
+++ Criminal Face Detection.py
@@ -5,40 +5,31 @@ from os import system
 from os import listdir
 from os.path import isfile, join
 from pickle import dump, load
-from tkinter import *
+#from tkinter import *
 
 face_cascade = cv2.CascadeClassifier(cv2.haarcascades + 'haarcascade_frontalface_default.xml')
 model = cv2.face_LBPHFaceRecognizer.create()
-plot = []
+plot = []          # Plot Variable storing Confidence plot
 criminals = []  # List Containing Criminal Names
-record = [0]  # List Containing Criminal Labels
-photo_count = 0
-url = ""
+
 
 '''Loading Criminals File with Pickle'''
 try:
-    if (isfile("criminals.pickle")):  ## Checking Saved Files
+    if (isfile("criminals.pickle")):          ## Checking Saved Files
         with open("criminals.pickle", "rb") as f:
             criminals = load(f)
-            record = load(f)
-            photo_count = record[-1]
-    else:  ## Create required directories
+    else:                                   ## Create required directories
         system("copy NUL criminals.pickle")
         system("mkdir data")
         system("mkdir model")
-        system("copy /Y  NUL model\\trained.xml")
+        system("copy /Y NUL model\\trained.xml")
 except:
     pass
 
 
-def get_name(id):  # Funtion Returning Criminal Name by ID
-    for index, i in enumerate(record, start=0):
-        if (i > id):
-            return criminals[(index - 1)]
 
-
-def enroll_data(name):
-    global model, photo_count, url
+def enroll_data(name,url=""):
+    global model
 
     ## Checking if URL exists
     if url is not "":
@@ -55,34 +46,41 @@ def enroll_data(name):
         bool, img = cap.read()
         if (bool == False):
             break
+        cv2.putText(img, "Press 'q' to Quit!", (0, 20), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0))
+        #img = cv2.resize(img, (0, 0), fx=0.8, fy=0.8)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=6)  ## Detect Faces Using Cascade
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=6)  ## Detect Faces Using Cascade
         if faces is not ():  ## if Face exists
             path = 'data/' + name + "/" + name + '-' + str(count) + '.jpg'  ## Path to store Face
-            cv2.putText(img, "Collected Samples:" + str(count), (0, 25), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))
+            cv2.putText(img, "Taking Samples: " + str(count), (0, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0))
             for x, y, w, h in faces:
                 cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
                 #if cv2.waitKey(1) & 0xFF == ord(' '):
                 count = count + 1
                 face = gray[y:y + h, x:x + w]
-                face = cv2.resize(face, (200, 200))  # Resize to 200x200
+                face = cv2.resize(face, (200,200))  # Resize image
                 cv2.imwrite(path, face)  # Save Image to path
 
 
-        cv2.resize(img, (1024, 2000))
+        img = cv2.resize(img, (1024,700))
         cv2.imshow("Enrolling Criminal ", img)
         if (cv2.waitKey(1) & 0xFF == ord('q')):
             # cv2.destroyWindow("Enrolling Criminal ")
             break
     cap.release()
     cv2.destroyAllWindows()
-    record.append((count + record[-1]))
     print("---------------------------------------------")
     print("       Samples Collected Successfully")
     print("---------------------------------------------\n")
     print("\n   Training Model...................\n")
     path_train = "data/" + name + "/"
     train_data, labels = [], []
+    criminal_id = 0 
+    if(name not in criminals):
+        criminals.append(name)      ## Add if Criminal is New
+        criminal_id = len(criminals)  #Generate new unique ID
+    else:
+        criminal_id = criminals.index(name)+1   #Use Existing ID
     files = [f for f in listdir(path_train) if isfile(join(path_train, f))]  ##Finding FaceData Files
     # print(files)
     if (files is []):
@@ -92,20 +90,20 @@ def enroll_data(name):
     for i, file in enumerate(files, start=0):
         img_path = path_train + '/' + files[i]
         img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-        train_data.append(np.asarray(img, dtype=np.uint8))  # Appending Face Data to
-        labels.append((photo_count + i))
+        train_data.append(np.asarray(img, dtype=np.uint8))  # Appending Face Data to train set
+        labels.append(criminal_id)
     labels = np.asarray(labels, dtype=np.int32)
-    photo_count = photo_count + count
     # model = cv2.face_LBPHFaceRecognizer.create()
     try:  # Try updating Model If exists
-        model.read('model/trained.xml')
+        model.read('model/trained.xml')                             # Update Previous Model
         model.update(np.asarray(train_data), np.asarray(labels))
     except:
-        model.train(np.asarray(train_data), np.asarray(labels))  # Train New Model
+        print("Creating New Model")
+        model.train(np.asarray(train_data), np.asarray(labels))          # Train New Model
     model.write('model/trained.xml')
     # system("del /Q data\*.jpg")
-    if name not in criminals:
-        criminals.append(name)  ## Add if Criminal is New
+
+
 
     print("Criminals Added : ", criminals)
     print("\n------------------------------------------------")
@@ -113,20 +111,18 @@ def enroll_data(name):
     print("------------------------------------------------\n\n")
 
 
-def detect():
-    global criminals, model, url
-    if url is not "":
+def detect(url=""):
+    global criminals, model
+    if url is not "":                   #if URL exists Read from URL
         cap = cv2.VideoCapture(url)
-        if (not cap.isOpened()):
-            print("URL doesn't work !!\n Try a working url")
-            return
     else:
-        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)   #Else Capture with WebCam
+    if (not cap.isOpened()):
+        print("URL or Webcam isn't working !!\n")
+        return
+    total_criminals = len(criminals)
     try:
-        if (len(criminals) == 0):
-            print("Please add criminals to continue!!")
-            return
-        model.read('model/trained.xml')
+        model.read('model/trained.xml')         # Read Model
     except:
         print("Model Doesn't Exist!!")
         return
@@ -136,39 +132,32 @@ def detect():
         bool, img = cap.read()
         if (bool == False):
             break
+        #img = cv2.resize(img, (0, 0), fx=0.7, fy=0.7)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=8)
+        cv2.putText(img, "Press 'q' to Quit!", (0, 20), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0))
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=6)
         if faces is not ():
             for x, y, w, h in faces:
                 roi = gray[y:y + h, x:x + w]
-                roi = cv2.resize(roi, (200, 200))
+                roi = cv2.resize(roi, (200,200)) # resizing Image
                 try:
                     result = model.predict(roi)
                     confidence = float('{0:.2f}'.format(100 * (1 - (result[1]) / 300)))
-                    if (result[0] < photo_count and result[0] >= 0 and confidence > 88):  # If ID is within label range
-                        plot.append(confidence)
-                        name = get_name(result[0])
+                    plot.append(confidence)
+                    if (result[0] > 0 and result[0] <= total_criminals and confidence > 85):  # If ID is within label range
+                        name = criminals[result[0] - 1]         #Getting Name from ID (result[0])
                         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
                         cv2.putText(img, str(confidence), (x, y - 10),cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))
-                        cv2.putText(img, name, (x, y + h + 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
-                        # label=""
-                        # label = str.zfill(str(result[0]), 2)
-                        # label = label + " " + str(confidence)
-
-                        #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                        #if (result[1] < 58):
-
-                        #else:
-                        #    cv2.putText(img, str(confidence), (x, y - 10),
-                        #                cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 255))
+                        cv2.putText(img,name, (x, y + h + 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
 
                     else:  # If Unknown Face
-                        plot.append(confidence)
                         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                        cv2.putText(img,str(confidence), (x, y + h + 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255,0), 2)
+
                 except Exception as e:
                     print(e)
 
-        cv2.resize(img, (1024, 728))
+        img = cv2.resize(img, (1024,700))
         cv2.imshow("Face Detector", img)
         if (cv2.waitKey(1) & 0xFF == ord('q')):
             cv2.destroyWindow("Face Detector")
@@ -186,7 +175,7 @@ def train_unknowns():
         img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
         img = cv2.resize(img, (200, 200))
         train_data.append(np.asarray(img, dtype=np.uint8))
-        labels.append((-i - 1))
+        labels.append((999))
     labels = np.asarray(labels, dtype=np.int32)
     # print("labels=",np.asarray(labels))
     # model = cv2.face_LBPHFaceRecognizer.create()
@@ -199,20 +188,20 @@ def train_unknowns():
 
 
 def main():
-    global plot, criminals, record, photo_count, url
+    global plot, criminals
     while (True):
         print("-------------------------------------------------------")
         print("         Criminal Face Identification System:")
         print("-------------------------------------------------------")
-        print("              1.Enroll Criminal Face")
+        print("              1.Register Criminal Face")
         print("              2.Detect Criminal Face")
         print("              3.Delete Model")
-        print("              4.Install Requirements(Run this Once)")
-        print("              5.Train with Unknowns(Run this Once)")
-        print("              6.Quit('q')")
+        print("              4.Train with Unknowns(Run this Once)")
+        print("              5.Quit('q')")
         print("______________________________________________________________")
         print("Criminal Data: ")
         if (len(criminals)):
+            print("Total Registered Criminals: ",len(criminals))
             print("{ ", criminals, " }")
         else:
             print("No criminal Registered!!")
@@ -221,26 +210,26 @@ def main():
 
         if opt == '1':
             name = input("Enter Criminal Name:")  # Getting Criminal Name
-            print("1.Input Criminal Footage URL")
+            print("\nRegister Criminal From?\n1.Input Criminal Footage URL")
             print("2.Use Webcam (Default)")
             opt = input("Enter Option: ")  # Input Feed
-            url = ""
             if (opt == '1'):
                 url = input("Enter URL:")
-
-            enroll_data(name)
+            enroll_data(name,url)
             with open("criminals.pickle", "wb") as f:
                 dump(criminals, f)
-                dump(record, f)
 
         if opt == '2':
-            print("1.Input Video URL")
+            if (len(criminals) == 0):
+                input("Please add criminals to Continue!!\n")
+                continue
+            print("\nDetect Criminal From?\n1.Input Video URL")
             print("2.Use Webcam (Default)")
             opt = input("Enter Option: ")
             url = ""
             if (opt == '1'):
                 url = input("Enter URL:")
-            detect()
+            detect(url)
             try:
                 if (plot is not []):
                     print("Highest Accuracy Generated is " + str((np.max(plot))))
@@ -261,26 +250,15 @@ def main():
             system("mkdir data")
 
             criminals = []
-            record = [0]
-            photo_count = 0
             plot = [0]
 
             print("\n\n\nSuccessfully Deleted Training Model")
         if opt == '4':
-            try:
-                system("pip install opencv-contrib-python")
-                system("pip install numpy")
-                system("pip install pickle")
-                print("\nSuccessfully Installed Requirements!\n")
-            except Exception:
-                print("\nFailed To Install!\n")
-
-        if opt == '5':
             print("Training with Unknowns.........")
             train_unknowns()
             print("training complete")
 
-        if opt == '6':
+        if opt == '5':
             break
 
 '''
